319ea588de6b700247e9728f3780384f30f7d6de
Dev1 <amit.agarwal@yendigital.com>
2020-06-06 20:10:28 +0530

    [AA]Code updated

---
+++ ApnaKaryala.Web/Controllers/LeaveController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApnaKaryala.Web.Controllers
+{
+    public class LeaveController : Controller
+    {
+        // GET: Leave
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
